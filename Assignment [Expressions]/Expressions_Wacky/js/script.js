98c6aba57da3fd1b81b32c75f12a78aa381e01cc
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 17:50:59 -0500

    Made major changes: Now prompts for height in feet, remaining inches, and arm length in inches

--- Assignment [Expressions]/Expressions_Wacky/js/script.js
+++ Assignment [Expressions]/Expressions_Wacky/js/script.js
@@ -3,10 +3,15 @@
 //Wacky - Since I am a huge fan of basketball and I've always wanted to know how high I need to jump for me to be able to dunk the ball, I'm creating a calculator that tells you the vertical leap you need to reach in order to dunk based on the user input of height.
 
 //Given
-var userMax = prompt("What is your max reach in inches? \n(max height of arms in the air, standing flat-footed)"); //This prompt ask the user's max reach height. This is the measurement of how high they can reach with with their hands in the air, standing flat-footed.
+var userHeight = prompt("What is your height in feet? (don't include inches)")
+var userHeight2 = prompt("How many inches remain?")
+var userArm = prompt("How long is one of your arms from shoulder to finger-tip in inches?")
+
 var rimHeight = 120; //Height of the basketball rim in inches
 
 //Result Variables
-var touchRim = rimHeight - userMax;
-var vertDunk = touchRim + 10; //You need to reach at least 10 inches above the rim in order to get the ball over the rim and throw it down!! 
-console.log("You need to jump " + touchRim + " inches in order to touch the rim and you need to jump " + vertDunk + " inches in order to dunk.");
+var totalHeight = userHeight * 12 + Number(userHeight2)
+var maxHeight = Number(totalHeight) + Number(userArm)
+var touchRim = rimHeight - maxHeight
+
+console.log(touchRim)
