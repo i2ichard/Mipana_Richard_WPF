4c120848f58ccb952b8949f32fafebe6d821f975
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 17:12:21 -0500

    Added result variable for how many inches you need to jump in order to touch the hoop.

--- Assignment [Expressions]/Expressions_Wacky/js/script.js
+++ Assignment [Expressions]/Expressions_Wacky/js/script.js
@@ -3,4 +3,9 @@
 //Wacky - Since I am a huge fan of basketball and I've always wanted to know how high I need to jump for me to be able to dunk the ball, I'm creating a calculator that tells you the vertical leap you need to reach in order to dunk based on the user input of height.
 
 //Given
-var userInput = prompt("How tall are you in inches?")
+var userMax = prompt("What is your max reach in inches? \n(max height of arms in the air, standing flat-footed)"); //This prompt ask the user's max reach height. This is the measurement of how high they can reach with with their hands in the air, standing flat-footed.
+var rimHeight = 120; //Height of the basketball rim in inches
+
+//Result Variables
+var touchRim = rimHeight - userMax
+console.log(touchRim)
