80daef34f9cf96c29029a41021e0052a6f9faf0a
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 15:21:09 -0500

    Personal calculator completed. Work in progress

--- Assignment [Expressions]/js/script.js
+++ Assignment [Expressions]/js/script.js
@@ -1,1 +1,19 @@
-//comment
+//This is a calculator for three real-life examples. The first is a calculator for a PERSONAL day-to-day use. The second is an INDUSTRY-specific calculator; one that can be used for industry related task. The third is a calculator for FUN; something that is wacky and out there.
+
+//Personal - I want to create a calculator that can track how much money I spend annually on eating out. My givens will be the amount of money I spend on food each week for 5 weeks.
+
+//Given
+
+
+var eatWeek = [23.99, 32.83, 38.50, 27.33, 40.21];
+
+//Result Variables
+
+var eatTotal = eatWeek[0] + eatWeek[1] + eatWeek[2] + eatWeek[3] + eatWeek[4];
+var eatAvg = eatTotal / 5;
+var eatAnnual = 52.1775 * eatAvg;
+
+
+//Print out how much money I spend annually on eating out using console.log.
+console.log("Based on the amount of money I spend on eating out each week for 5 weeks ($" + eatAvg + ")," + " I spend about $" + eatAnnual + " each year on eating out.");
+
