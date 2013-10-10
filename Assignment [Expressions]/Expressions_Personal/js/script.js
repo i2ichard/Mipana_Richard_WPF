cdcbf483aacf81cacfe88cff19b16d087aadf097
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 15:37:41 -0500

    Updated heading for index.html and script.js in Expressions_Personal

--- Assignment [Expressions]/Expressions_Personal/js/script.js
+++ Assignment [Expressions]/Expressions_Personal/js/script.js
@@ -1,3 +1,4 @@
+//Richard Mipana, 10/10/2013, Assignment-[Expressions]
 //This is a calculator for three real-life examples. The first is a calculator for a PERSONAL day-to-day use. The second is an INDUSTRY-specific calculator; one that can be used for industry related task. The third is a calculator for FUN; something that is wacky and out there.
 
 //Personal - I want to create a calculator that can track how much money I spend annually on eating out. My givens will be the amount of money I spend on food each week for 5 weeks.
