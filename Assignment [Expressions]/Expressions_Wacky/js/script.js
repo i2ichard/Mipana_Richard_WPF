77af6b4c9539e9fb7736b2048220af78d0ee9f81
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 18:04:22 -0500

    Updated final wacky calculator: includes more prompts and shows an alert of the final answer after prompts.

--- Assignment [Expressions]/Expressions_Wacky/js/script.js
+++ Assignment [Expressions]/Expressions_Wacky/js/script.js
@@ -3,15 +3,17 @@
 //Wacky - Since I am a huge fan of basketball and I've always wanted to know how high I need to jump for me to be able to dunk the ball, I'm creating a calculator that tells you the vertical leap you need to reach in order to dunk based on the user input of height.
 
 //Given
-var userHeight = prompt("What is your height in feet? (don't include inches)")
-var userHeight2 = prompt("How many inches remain?")
-var userArm = prompt("How long is one of your arms from shoulder to finger-tip in inches?")
+var userHeight = prompt("What is your height in feet? (don't include inches)");
+var userHeight2 = prompt("How many inches remain?");
+var userArm = prompt("How long is one of your arms from shoulder to finger-tip in inches?");
 
-var rimHeight = 120; //Height of the basketball rim in inches
+var rimHeight = 120; //Standart height of the basketball rim in inches
 
 //Result Variables
-var totalHeight = userHeight * 12 + Number(userHeight2)
-var maxHeight = Number(totalHeight) + Number(userArm)
-var touchRim = rimHeight - maxHeight
+var totalHeight = userHeight * 12 + Number(userHeight2); //This is the total height of the user in inches.
+var maxHeight = Number(totalHeight) + Number(userArm); //This is the max height reach of the user in inches.
+var touchRim = rimHeight - maxHeight; //The height in inches that the user needs to jump vertically to touch a standard height basketball rim.
+var vertDunk = touchRim + 10; //You need to reach at least 10 inches above the rim to get the ball over and throw it down!!
+alert("You need to jump at least " + touchRim + " inches vertically in order to touch the rim and jump at least " + vertDunk + " inches vertically in order to throw it down!!!" )
 
-console.log(touchRim)
+console.log(vertDunk)
