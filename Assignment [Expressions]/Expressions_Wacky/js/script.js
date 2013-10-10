e6a142361f6307615b77b214593760a317d8ddef
i2ichard <richard.mipana@fullsail.edu>
2013-10-10 16:56:05 -0500

    Updated givens

--- Assignment [Expressions]/Expressions_Wacky/js/script.js
+++ Assignment [Expressions]/Expressions_Wacky/js/script.js
@@ -1,3 +1,6 @@
 //This is a calculator for three real-life examples. The first is a calculator for a PERSONAL day-to-day use. The second is an INDUSTRY-specific calculator; one that can be used for industry related task. The third is a calculator for FUN; something that is wacky and out there.
 
 //Wacky - Since I am a huge fan of basketball and I've always wanted to know how high I need to jump for me to be able to dunk the ball, I'm creating a calculator that tells you the vertical leap you need to reach in order to dunk based on the user input of height.
+
+//Given
+var userInput = prompt("How tall are you in inches?")
