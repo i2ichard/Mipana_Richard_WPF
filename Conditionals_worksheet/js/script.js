83b9e7a19a617bfb0da769824373071dc647c297
i2ichard <richard.mipana@fullsail.edu>
2013-10-17 13:08:52 -0500

    Completed "Last Chance for Gas!" problem

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -16,3 +16,28 @@ if(compWeight >= 250){
 	//if less than 250 pounds
 	console.log("The competitor needs to gain some weight!");
 }
+
+
+//Last Chance for Gas!
+//A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+
+//Given: Gas efficiency (in mpg), amount of gas in tank (in %), gas tank capacity (in gallons), distance until next gas station (in miles)
+
+var carEcon = 32
+var gasHave = 50
+var tankCap = 14
+var nextGas = 200 //This is the distance (in mile) until the next gas station.
+
+var gasInTank = gasHave * tankCap / 100 //This is the amount of gas left in the tank (in gallons) based on the percentage in gasHave and the capacity in var tankCap.
+var carRange = gasInTank * carEcon //This is the range that the car can travel with the gas it currently has in the tank before it reaches empty.
+
+
+//Result: "Yes, you can make it without stopping for gas!" or "You only have X gallons of gas in your tank, better get gas now while you can!"
+
+if(carRange > nextGas){
+	//if the car can make it to the next gas station with the gas it currently has
+	console.log("Yes, you can make it without stopping for gas!");
+}else{
+	//if the car cannot make it to the next gas station with the gas it currently has.
+	console.log("You only have " + gasInTank + " gallons of gas in your tank, better get gas now while you can!");
+}
