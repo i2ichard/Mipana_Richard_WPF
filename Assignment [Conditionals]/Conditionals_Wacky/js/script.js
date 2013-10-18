70185e949c6682b422d5c42d3dc4b5eeaf27abf1
i2ichard <richard.mipana@fullsail.edu>
2013-10-17 21:11:26 -0500

    used ternary

--- Assignment [Conditionals]/Conditionals_Wacky/js/script.js
+++ Assignment [Conditionals]/Conditionals_Wacky/js/script.js
@@ -1,1 +1,11 @@
 //Richard Mipana, 10/17/2013, Assignment[Conditionals] - Wacky
+
+//Coffee me
+//I want to calculate if I will need coffee to get myself through the day based on the amount of hours I slept the night before and the amount of hours I will be working for the day.
+
+//Given
+var hourSlept = prompt("How many hours did you sleep?");
+var hoursWork = prompt("How many hours will you be working today?");
+
+//Result
+(hourSlept >= 6 && hoursWork <= 5) ? console.log("You could probably manage without coffee") : console.log("Go get yourself some coffee. You're gonna need it.")
