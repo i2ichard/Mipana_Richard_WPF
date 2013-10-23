fa02521bf255f125f6a694e8b86de4d267761cb7
i2ichard <richard.mipana@fullsail.edu>
2013-10-23 16:21:09 -0500

    testing calculations for train fare formula

--- Assignment [Functions]/Functions_Personal/js/script.js
+++ Assignment [Functions]/Functions_Personal/js/script.js
@@ -6,17 +6,19 @@
 
 //Given: The average amount of days I work in a month (23), My car's gas mileage (23 mpg), My car's gas tank capacity (17.1 gallons), How many miles I drive to and from work (75.8 miles)
 
-function calcCost(X){
+function calcCost(X, Y){
 	var daysWork = 23;
 	var carMPG = 23;
 	var carTank = 17.1;
 	var carRange = carMPG * carTank;
 	var milesDrive = 75.8;
 	
-//Parameters: Price of gas per gallon (in dollars), Price of round-trip train fair (in dollars).
+//Parameters: Price of gas per gallon (in dollars), Price of round-trip train fare (in dollars).
 	var gasMonth = (milesDrive * daysWork / carRange) * carTank * X;
-	console.log(gasMonth);
+	var trainMonth = daysWork * Y;
+//Result: Print to the console, "With the gas price at $X, each month I'll spend $ 'gasMonth'. The train fare is $Y, so I would spend $'trainMonth' per month if i were to take the train."	
+	console.log("With the gas price at $" + X + ", each month I'll spend $" + gasMonth + " on gas. Alternatively, the train fare is $" + Y +", so I would spend $" + trainMonth + " per month if i were to take the train for the month.");
 	
 }
 
-calcCost(3.45)
+calcCost(3.50, 10.00);
