0d4cc4b10f58c381b1647002dd49df9558603137
i2ichard <richard.mipana@fullsail.edu>
2013-10-23 17:03:37 -0500

    Came up with what I want to calculate for my wacky calculator

--- Assignment [Functions]/Functions_Wacky/js/script.js
+++ Assignment [Functions]/Functions_Wacky/js/script.js
@@ -1,1 +1,6 @@
 //Richard Mipana, 10/24/2013, Assignments_[Functions] - Wacky Calculator
+
+//Doggie Boot Camp.
+
+//My dog Nigel is fat. I want him to loose 5 pounds but I only have a limited amount of time each day after work that I can walk him. I want to calculate how long it will take for him to lose 5 pounds while doing it in a healthy fashion. I did some research on Google and found that for my dog's weight (77 lbs.), a healthy daily caloric intake is around 1800 calories. I also found that in order to lose a single pound, he'd have to burn 3500 calories.
+
