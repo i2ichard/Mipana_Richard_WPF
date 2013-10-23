446d1b6ffe8ff79f83dbe9fc24773fd160fe4bc3
i2ichard <richard.mipana@fullsail.edu>
2013-10-23 16:04:25 -0500

    Testing out preliminary formula for how much money is spent on gas per month

--- Assignment [Functions]/Functions_Personal/js/script.js
+++ Assignment [Functions]/Functions_Personal/js/script.js
@@ -4,16 +4,19 @@
 
 //Every day, I commute from my town of Naperville, Illinois all the way to Chicago for work. I want to calculate which method of transportation would be more cost efficient, driving or taking a train over the span of a month.
 
-//Given: The average amount of days I work in a month (23), My car's gas mileage (23 mpg), My car's gas tank capacity (17.1 gallons)
+//Given: The average amount of days I work in a month (23), My car's gas mileage (23 mpg), My car's gas tank capacity (17.1 gallons), How many miles I drive to and from work (75.8 miles)
 
-function calcCost(){
-	var daysWork = 23
-	var carMPG = 23
-	var carTank = 17.1
-	var carRange = carMPG * carTank
+function calcCost(X){
+	var daysWork = 23;
+	var carMPG = 23;
+	var carTank = 17.1;
+	var carRange = carMPG * carTank;
+	var milesDrive = 75.8;
 	
 //Parameters: Price of gas per gallon (in dollars), Price of round-trip train fair (in dollars).
-	var gasMonth = 23
-	
+	var gasMonth = (milesDrive * daysWork / carRange) * carTank * X;
+	console.log(gasMonth);
 	
 }
+
+calcCost(3.45)
