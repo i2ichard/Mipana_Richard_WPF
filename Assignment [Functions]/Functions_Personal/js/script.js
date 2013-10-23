0597235ba42e36c6eeb97f4dfacfcde9f74df273
i2ichard <richard.mipana@fullsail.edu>
2013-10-23 15:49:47 -0500

    Setting up parameters and givens for personal calculator

--- Assignment [Functions]/Functions_Personal/js/script.js
+++ Assignment [Functions]/Functions_Personal/js/script.js
@@ -1,1 +1,19 @@
 //Richard Mipana, 10/24/2013, Assignments_[Functions] - Personal Calculator
+
+//Car vs. Train
+
+//Every day, I commute from my town of Naperville, Illinois all the way to Chicago for work. I want to calculate which method of transportation would be more cost efficient, driving or taking a train over the span of a month.
+
+//Given: The average amount of days I work in a month (23), My car's gas mileage (23 mpg), My car's gas tank capacity (17.1 gallons)
+
+function calcCost(){
+	var daysWork = 23
+	var carMPG = 23
+	var carTank = 17.1
+	var carRange = carMPG * carTank
+	
+//Parameters: Price of gas per gallon (in dollars), Price of round-trip train fair (in dollars).
+	var gasMonth = 23
+	
+	
+}
