68c59d5e013af9b647762834fdaab81b0d2c7dbc
i2ichard <richard.mipana@fullsail.edu>
2013-10-23 13:36:08 -0500

    Completed circumference calculator

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,5 +2,19 @@
 
 
 //Circumference - Calculate the circumference of a circle.
-//Given - Radius of the circle
+
+
+function calcCirc(r){
+	//Parameter: Radius of the circle
+	var pi = Math.PI;
+	//Return: The calculated circumference of a circle using the parameter, radius.
+	var circum = 2*pi*r; //The formula for calculating the circumference of a circle is 2*pi*radius.
+	//Result: Print to the console, "The circumference of the circle is X"
+	console.log("The circumference of the circle is " + circum + ".");
+}
+calcCirc(9);
+calcCirc(1);
+
+
+
 
