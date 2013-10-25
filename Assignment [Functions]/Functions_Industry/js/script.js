6776bd739f72d3e52594dd10255ed0cdb0a6f55c
i2ichard <richard.mipana@fullsail.edu>
2013-10-24 21:22:48 -0500

    set-up industry calculator

--- Assignment [Functions]/Functions_Industry/js/script.js
+++ Assignment [Functions]/Functions_Industry/js/script.js
@@ -1,1 +1,13 @@
 //Richard Mipana, 10/24/2013, Assignments_[Functions] Industry Calculator
+
+//A huge trend in the web development field right now is responsive web design. Web sites that adapt responsive web design have web pages that automatically resize its content to be optimally viewed on what ever device you are using. I want to calculate the ratio that the content is being resized to with the desktop version of the website being 1:1.
+
+//Given: Screen size of the device in inches.
+
+var iphone = 4 //This is the size of the iphone screen.
+var android = 4.7
+var ipad = 9.7
+var desktop = 23
+
+
+
