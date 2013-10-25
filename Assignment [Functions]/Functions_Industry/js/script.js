bb130ecb46af2263a066ee65f3e1d4bec101c0fe
i2ichard <richard.mipana@fullsail.edu>
2013-10-24 21:44:23 -0500

    changed the calculator to a screen size percentage calculator. completed industry calculator

--- Assignment [Functions]/Functions_Industry/js/script.js
+++ Assignment [Functions]/Functions_Industry/js/script.js
@@ -1,13 +1,13 @@
 //Richard Mipana, 10/24/2013, Assignments_[Functions] Industry Calculator
 
-//A huge trend in the web development field right now is responsive web design. Web sites that adapt responsive web design have web pages that automatically resize its content to be optimally viewed on what ever device you are using. I want to calculate the ratio that the content is being resized to with the desktop version of the website being 1:1.
+//A huge trend in the web development field right now is responsive web design. Web sites that adapt responsive web design have web pages that automatically resize its content to be optimally viewed on what ever device you are using. I want to calculate the ratio that the content is being resized to with the desktop version of the website being 100%
 
-//Given: Screen size of the device in inches.
+//Given: Screen size of the device in inches. Lets just say that the average desktop size is 23 inches
 
-var iphone = 4 //This is the size of the iphone screen.
-var android = 4.7
-var ipad = 9.7
-var desktop = 23
+function calcScreenRatio(X){
+	var desktop = 23;
+	var screenSz = X / desktop * 100;
+	console.log("If the size of your screen is " + X + " inches, then the percentage of the web page is " + screenSz + "% of the original desktop version.");
+}
 
-
-
+calcScreenRatio(4);
